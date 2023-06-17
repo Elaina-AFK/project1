abb22580af993c7d141f03ccd365537523b65d18
Nonoel <55495372+Elaina-AFK@users.noreply.github.com>
2023-06-17 14:02:10 +0700

    ✨ add change theme features and another new theme

--- web/js/utils/loginPage.js
+++ web/js/utils/loginPage.js
@@ -33,49 +33,42 @@ function createElementWithId(nodetype, id) {
 }
 
 function themeTogglerNode() {
-  const node = createElementWithId("div", "themeToggler");
-  themeHover(node);
-  node.addEventListener("click", () => {
-    switchCss();
-    themeHover(node);
-  });
+  const node = document.createElement("div");
+  node.className = "navbar";
+  node.appendChild(themeList());
   return node;
 }
 
-function switchCss() {
+function themeList() {
+  const ul = document.createElement("ul");
+  ul.className = "navbar-nav";
+  ul.appendChild(addTheme("#D291BC", "css1"));
+  ul.appendChild(addTheme("#00adb5", "css2"));
+  ul.appendChild(addTheme("#FFAFCC", "css3"));
+  return ul;
+}
+
+function addTheme(terColor, styleId) {
+  const li = document.createElement("li");
+  li.className = "nav-item";
+  const div = document.createElement("div");
+  div.className = "themeToggler";
+  div.style.backgroundColor = terColor;
+  div.addEventListener("click", () => {
+    disableAllCss();
+    document.getElementById(styleId).disabled = false;
+  });
+  li.appendChild(div);
+  return li;
+}
+
+function disableAllCss() {
   const css1 = document.getElementById("css1");
   const css2 = document.getElementById("css2");
-
-  if (css1.disabled) {
-    css1.disabled = false;
-    css2.disabled = true;
-  } else {
-    css1.disabled = true;
-    css2.disabled = false;
-  }
+  const css3 = document.getElementById("css3");
+  css1.disabled = true;
+  css2.disabled = true;
+  css3.disabled = true;
 }
 
-function themeHover(themeNode) {
-  const base = getComputedStyle(document.body).getPropertyValue(
-    "--tertiary-color"
-  );
-  const css1 = "#D291BC";
-  const css2 = "#00adb5";
-
-  if (base === css1) {
-    themeNode.addEventListener("mouseover", () => {
-      themeNode.style.backgroundColor = css2;
-    });
-    themeNode.addEventListener("mouseout", () => {
-      themeNode.style.backgroundColor = css1;
-    });
-  } else if (base === css2) {
-    themeNode.addEventListener("mouseover", () => {
-      themeNode.style.backgroundColor = css1;
-    });
-    themeNode.addEventListener("mouseout", () => {
-      themeNode.style.backgroundColor = css2;
-    });
-  }
-}
 export default { loginPageNode, themeTogglerNode };
